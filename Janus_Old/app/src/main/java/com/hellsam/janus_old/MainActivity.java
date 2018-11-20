dc889bce6fe64237cf1211e38aa710ee91f1b441
hellsam <chenbs@getui.com>
2018-11-20 13:46:37 +0800

    init

---
+++ Janus_Old/app/src/main/java/com/hellsam/janus_old/MainActivity.java
@@ -0,0 +1,22 @@
+package com.hellsam.janus_old;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "I am new app.", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
---
+++ README.md
@@ -0,0 +1,29 @@
+# Janus漏洞模拟测试
+
+## 文件目录介绍
+
+Janus_Old文件夹：原始app的项目工程
+
+janus_old.apk：原始app项目工程打包并用janus_old.keystore签名的apk文件。
+
+classes.dex：破解者修改过的dex文件（这里为了简单其实是直接修改Janus_Old工程逻辑打包并解压得到的dex文件）
+
+janus.py：合并最终apk的脚本
+
+## 测试流程
+
+1. 安装janus_old.apk，点击按钮，Toast打印“I am old app.”，如下图
+
+	![](./janus_old.jpg)
+	
+2. 用janus.py脚本合并出最终apk，命令如下：
+
+	```
+	python janus.py classes.dex janus_old.apk janus_final.apk
+	```	
+	
+3. 覆盖安装并打开janus_finish.apk，点击按钮，截图如下：
+
+	![](./janus_final.jpg)
+	
+>> 测试机型：nexus5，android版本：5.1.1
